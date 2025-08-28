4e9735b1f26021d4fb141ac140021044f05777e1
CandyLOVO <1565630950@qq.com>
2025-08-28 12:23:27 +0800

    循环框架

--- PolicyGradient.py
+++ PolicyGradient.py
@@ -1,19 +1,23 @@
-import gymnasium as gym
-env = gym.make("CartPole-v1", render_mode="human")
-for i_episode in range(20):
-    # reset 现在返回 (observation, info)
-    observation, info = env.reset()
-    for t in range(100):  # 每局最多 100 步
-        # 渲染画面
-        env.render()
-        # 随机选择一个动作（左推 or 右推）
-        action = env.action_space.sample()
-        # step 现在返回 5 个值
-        observation, reward, terminated, truncated, info = env.step(action)
-        done = terminated or truncated  # 游戏是否结束
+from os import truncate
 
+import gymnasium
+from pygame.display import update
+
+env = gymnasium.make("CartPole-v1") #预设环境
+
+for episode in range(500):
+    observation = env.reset() #重置环境，初始状态观测空间
+    while True:
+        #算法
+        action = select_next_action(observation) #算法决定
+        ####
+        next_observation, reward, terminated, truncated, info = env.step(action) #执行动作，映射环境定义
+        done = terminated or truncate
         if done:
-            print("Episode finished after {} timesteps".format(t + 1))
             break
 
+        update(observation, action, reward, terminated) #算法更新模型参数
+        observation = next_observation #更新观测空间
+
 env.close()
+
