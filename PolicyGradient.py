7478366bb00ce5ed724f8e41b7333f0b47cc45c4
CandyLOVO <1565630950@qq.com>
2025-08-28 23:50:33 +0800

    能运行得到结果

--- PolicyGradient.py
+++ PolicyGradient.py
@@ -4,6 +4,8 @@ import torch
 import torch.nn as nn
 #神经网络函数接口
 import torch.nn.functional as F
+from fontTools.misc.cython import returns
+
 
 #状态->动作概率
 class Policy(nn.Module):
@@ -23,12 +25,12 @@ optimizer = torch.optim.Adam(policy.parameters(), lr=1e-3)
 
 gamma = 0.99 #折扣因子γ
 for episode in range(500): #episode一局
-    state = env.reset() #初始化环境[位置，速度，角度，角速度]
+    state, info = env.reset() #初始化环境[位置，速度，角度，角速度]
     log_prob = [] #logΠ(a|s)
     rewards = []
 
     while True:
-        #算法
+    #算法
         state_tensor = torch.tensor(state, dtype=torch.float) #转化为张量，PyTorch定义的神经网络只接收张量输入
         action_prob = policy.forward(state_tensor) #映射得概率，离散
 
@@ -37,11 +39,29 @@ for episode in range(500): #episode一局
 
         action = sampled_action.item() #索引转化成整数
         state, reward, terminated, truncated, info = env.step(action) #环境执行，动作索引->新状态，及时奖励，自然终止，截断，额外info
+        rewards.append(reward)  #更新reward
+
         done = terminated or truncated #两个终止标志合并
-
-        rewards.append(reward) #更新
         if done: break
-        ####
 
-env.close()
+    #策略梯度算法
+    returns = []
+    G_t = 0
+    for r in rewards[::-1]: #反向遍历
+        G_t = r + gamma * G_t
+        returns.insert(0, G_t) #恢复原顺序
+    returns = torch.tensor(returns) #转化为张量（同上）
+    returns = (returns - returns.mean()) / (returns.std() + 1e-5)  #去基线b/奖励标准差
+
+    loss = 0
+    for log_p, G in zip(log_prob, returns): #取变量赋值
+        loss = loss - G * log_p
+
+    optimizer.zero_grad() #梯度清零
+    loss.backward() #反向传播，自动计算loss对模型所有可训练参数的梯度
+    optimizer.step() #更新模型参数
+    ####
+    total_reward = sum(rewards)
+    print(f"Episode {episode}, total reward: {total_reward}")
+
 
