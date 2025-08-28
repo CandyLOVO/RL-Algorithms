3e7c7543657c685b575b5cce40ccda200a27e50e
CandyLOVO <1565630950@qq.com>
2025-08-28 18:19:56 +0800

    学完MLP，先整这部分:(

--- PolicyGradient.py
+++ PolicyGradient.py
@@ -1,16 +1,23 @@
 import gymnasium
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
 from pygame.display import update
 
 env = gymnasium.make("CartPole-v1") #预设环境
+
+
 
 for episode in range(500):
     observation = env.reset() #重置环境，初始状态观测空间
     while True:
         #算法
+        #状态->动作概率
         action = select_next_action(observation) #算法决定
         ####
         next_observation, reward, terminated, truncated, info = env.step(action) #执行动作，映射环境定义
-        done = terminated or truncate
+        done = terminated or truncated
         if done:
             break
 
