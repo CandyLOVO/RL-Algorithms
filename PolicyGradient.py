688d536b1b9b2f3528ed5cca4a2603588963d47a
CandyLOVO <1565630950@qq.com>
2025-08-29 12:21:14 +0800

    增加一层隐藏层

--- PolicyGradient.py
+++ PolicyGradient.py
@@ -11,19 +11,21 @@ class Policy(nn.Module):
     def __init__(self, state_dim=4, hidden_dim=36, action_dim=2): #隐藏层神经元数量暂定36 4*9
         super().__init__()
         self.fc1 = nn.Linear(state_dim, hidden_dim)
-        self.fc2 = nn.Linear(hidden_dim, action_dim)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, action_dim)
 
     def forward(self, x): #x必须是张量
         x = F.relu(self.fc1(x)) #输入映射ReUL
-        output = F.softmax(self.fc2(x), dim=-1) #dim=-1直接选中特征数，无需记忆索引
+        x = F.relu(self.fc2(x))
+        output = F.softmax(self.fc3(x), dim=-1) #dim=-1直接选中特征数，无需记忆索引
         return output #动作概率分布
 
 env = gymnasium.make("CartPole-v1")
-#env = gymnasium.make("CartPole-v1", render_mode="human") #预设环境
+# env = gymnasium.make("CartPole-v1", render_mode="human") #预设环境
 policy = Policy()
-optimizer = torch.optim.Adam(policy.parameters(), lr=0.003)
+optimizer = torch.optim.Adam(policy.parameters(), lr=0.001)
 
-gamma = 0.99 #折扣因子γ
+gamma = 0.8 #折扣因子γ
 all_rewards = []
 for episode in range(500): #episode一局
     state, info = env.reset() #初始化环境[位置，速度，角度，角速度]
